b17bdbc26324eea2bfce69bd920885f2c2ae4707
pioih86 <48486598+pioih86@users.noreply.github.com>
2019-05-13 20:13:14 +0300

    Update script.sql

--- src/ru/avalon/java/j30/labs/script.sql
+++ src/ru/avalon/java/j30/labs/script.sql
@@ -1,6 +1,54 @@
-/*
- * TODO(Студент): Опишите процесс создания базы данных
- * 1. Создайте все таблицы, согласно предложенной диаграмме.
- * 2. Опишите все необходимые отношения между таблицами.
- * 3. Добавьте в каждую таблицу минимум три записи.
- */
+create table roles
+(
+    id int unique,
+    name varchar(255) primary key
+);
+
+create table userinfo
+(
+    id int primary key,
+    name varchar(255),
+    surname varchar(255)
+);
+
+create table users
+(
+    id int unique,
+    password varchar(255),
+    email varchar(255) primary key, 
+    info int unique references userinfo(id),
+    roles int references roles(id)
+);
+
+create table supplier
+(
+    id int unique,
+    name varchar(255) primary key,
+    address varchar(255) not null,
+    phone varchar(255),
+    representative varchar(255) not null   
+);
+
+create table product 
+(
+    id int unique,
+    code varchar(255) primary key, 
+    title varchar(255),
+    supplier int references supplier(id),
+    initial_price double,
+    retail_value double
+);
+
+create table orders
+(
+    id int primary key,
+    users int references users(id),
+    created timestamp default current_timestamp
+);
+
+create table order2product
+(
+    orders int,
+    product int,
+    constraint pk_order_product primary key(orders, product)
+);
