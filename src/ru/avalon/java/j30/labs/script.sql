d80d99659880a64d8f47204c6621d993e1451359
pioih86 <48486598+pioih86@users.noreply.github.com>
2019-05-15 08:03:23 +0300

    Update script.sql

--- src/ru/avalon/java/j30/labs/script.sql
+++ src/ru/avalon/java/j30/labs/script.sql
@@ -1,55 +1,55 @@
-create table roles
+create table Roles
 (
     id int not null unique
     generated always as identity(start with 1, increment by 1),
-    name varchar(255) primary key
+    name varchar(255) not null primary key
 );
 
-create table userinfo
+create table UserInfo
 (
-    id int primary key,
+    id int not null primary key,
     name varchar(255),
     surname varchar(255)
 );
 
-create table users
+create table Users
 (
     id int not null unique,
+    email varchar(255) not null primary key,
     password varchar(255) not null,
-    email varchar(255) not null primary key, 
-    info int not null unique references userinfo(id),
-    roles int not null references roles(id)
+    info int not null unique references UserInfo(id),
+    role int not null references Roles(id)
 );
 
-create table supplier
+create table Orders
+(
+    id int not null primary key,
+    userid int not null references Users(id),
+    created timestamp not null default current_timestamp
+);
+
+create table Supplier
 (
     id int not null unique,
-    name varchar(255) primary key,
+    name varchar(255) not null primary key,
     address varchar(255),
     phone varchar(255) not null,
-    representative varchar(255)   
+    representative varchar(255)  
 );
 
-create table product 
+create table Product 
 (
     id int not null unique,
-    code varchar(255) primary key, 
+    code varchar(255) not null primary key, 
     title varchar(255) not null,
-    supplier int not null references supplier(id),
+    supplier int not null references Supplier(id),
     initial_price double not null,
     retail_value double not null
 );
 
-create table orders
-(
-    id int primary key,
-    users int not null references users(id),
-    created timestamp not null default current_timestamp
-);
-
 create table order2product
 (
-    orders int,
-    product int,
-    constraint pk_order_product primary key(orders, product)
+    orderid int not null references Orders(id),
+    product int not null references Product(id),
+    constraint pk_order_product primary key(orderid, product)
 );
